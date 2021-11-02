aa0294eb1be20a9291c747ec9d9f8c400c554b20
Samuel Panek <samuelrpanek@gmail.com>
2021-11-02 15:57:58 -0700

    creates functioning server on 3001

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
 require('dotenv').config();
 const cors = require('cors');
 const express = require('express');
+
+const app = express();
+
+app.use(cors());
+
+const PORT = process.env.PORT
+
+let sendHello = () => {
+    console.log('this stuff is so cool');
+}
+
+app.get('/hello',sendHello)
+
+
+
+
+app.listen(3001, () => console.log(`I'm A Live! And i am listen at port: ${PORT}`))
