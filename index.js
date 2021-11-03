be7549f05fdb364a7866b74338b213f2062ea65e
Samuel Panek <samuelrpanek@gmail.com>
2021-11-02 17:37:10 -0700

    adds Forecast class

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const cors = require('cors');
 const express = require('express');
+const weather = require('./data/weather.json')
 
 const app = express();
 
@@ -8,12 +9,51 @@ app.use(cors());
 
 const PORT = process.env.PORT
 
-let sendHello = () => {
+let sendHello = (req,res) => {
     console.log('this stuff is so cool');
 }
 
-app.get('/hello',sendHello)
+class Forecast {
+    constructor(date,description){
+        this.date = date;
+        this.description = description;
+    }
 
+}
+
+let createDescription = (element) => {
+    let des = `"description": "Low of ${element.low_temp}, high of ${element.high_temp} with ${element.weater.description}"`
+    return des;
+}
+
+let searchWeather = (req,res) => {
+    let searchReference = req.query;
+    let cityResponse = weather.find(element => {
+
+        if(element.city_name.toLowerCase() === searchReference.city_name.toLowerCase() && Math.floor(element.lat) == Math.floor(searchReference.lat) && Math.floor(element.lon) == Math.floor(searchReference.lon)){
+            let dayForecast = [];
+
+            element.data.map(element => {
+                let des = createDescription(element);
+                let date = element.valid_date;
+                dayForecast.push(new Forecast(date,des));
+            });
+
+            res.send(dayForecast);
+            res.send(console.log(element));
+        }
+        else {
+            res.status(403).send('not found')
+        }
+    });
+
+
+
+}
+
+
+app.get('/hello',sendHello)
+app.get('/weather',searchWeather)
 
 
 
